c544690c9f6d947e23e9fb1f84000602787cdd74
bordyale <alessio.bordignon@gmail.com>
2018-01-31 15:33:20 +0100

    script sql

---
+++ varie/scripts/deleteProduct.sql
@@ -0,0 +1,6 @@
+
+
+
+delete from `PRODUCT_KEYWORD_NEW` where `PRODUCT_ID`='GC-001'; 
+delete from `PRODUCT_CONTENT` where `PRODUCT_ID`='GC-001'; 
+delete from `PRODUCT` where `PRODUCT_ID`='GC-001'; 
